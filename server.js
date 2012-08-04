bec3babeca60710b8fb862f8243e5bde3654ecb3
Mark Eggers <mark_eggers@hotmail.com>
2012-08-04 19:20:15 +0100

    Testing GIT uploads to Azure

--- server.js
+++ server.js
@@ -3,5 +3,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Azure World\n');
+  res.end('Hello There Azure World\n');
 }).listen(process.env.PORT || 3000);
